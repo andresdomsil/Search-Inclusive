09901f2585003c829db2d7391a02d81d811937e4
andresdomsil <andres_domin_silva@outlook.com>
2017-05-30 20:51:06 -0500

    Implementacion de Geolocalización e instalacion de google Maps con sus KEYS

--- src/pages/lugar/lugar.ts
+++ src/pages/lugar/lugar.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Http } from "@angular/http";
+import { Geolocation } from '@ionic-native/geolocation';
 /**
  * Generated class for the LugarPage page.
  *
@@ -17,7 +18,7 @@ export class LugarPage {
   public title    : any;
   public lugar    : any[];
   public imagenes : any[];
-  constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public http: Http, public geolocation: Geolocation) {
   }
 
   ionViewDidLoad() {
@@ -42,6 +43,18 @@ export class LugarPage {
 
   cerrar(){
     this.navCtrl.pop();
+  }
+
+  ubicacion(){
+    this.geolocation.getCurrentPosition().then((resp) => {
+      console.log(resp.coords.latitude);
+      console.log(resp.coords.longitude);
+      
+    // resp.coords.latitude
+    // resp.coords.longitude
+    }).catch((error) => {
+      console.log('Error getting location', error);
+    });
   }
 
 }
